c9507bce7e8ee3f19f8ed22dde05af2de0f3775d
Pankaj Verma <pv62012@gmail.com>
2020-09-09 16:08:34 +0530

    Updates

---
+++ precache-manifest.5676326b36950365e1fe630c39ed22e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552b988fb13782cab92fc583790fdd07",
+    "url": "/firstreactwebsite/index.html"
+  },
+  {
+    "revision": "ba97a1064dffc1b557d3",
+    "url": "/firstreactwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "91d73243086a1d9f1d30",
+    "url": "/firstreactwebsite/static/css/main.d5fe2817.chunk.css"
+  },
+  {
+    "revision": "ba97a1064dffc1b557d3",
+    "url": "/firstreactwebsite/static/js/2.6ebcf1b3.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/firstreactwebsite/static/js/2.6ebcf1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d73243086a1d9f1d30",
+    "url": "/firstreactwebsite/static/js/main.eea9f72a.chunk.js"
+  },
+  {
+    "revision": "0c4dda8c96e0b9b55ff3",
+    "url": "/firstreactwebsite/static/js/runtime-main.cfacfd67.js"
+  },
+  {
+    "revision": "bbef40c0f98fc78ed23945501e986214",
+    "url": "/firstreactwebsite/static/media/brighten.bbef40c0.jpg"
+  }
+]);
